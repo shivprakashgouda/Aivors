3181ddf09e6f90901fc9da5734e0ca47292f6992
shivprakashgouda <157306802+shivprakashgouda@users.noreply.github.com>
2025-11-06 09:35:45 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -106,12 +106,13 @@ export const Navigation = ({ onSignInClick, onBookDemoClick }: NavigationProps) 
                 Admin Login
               </Button>
             </Link> */}
-            {/* <Button
+            <a href="">
+             <Button
               onClick={onBookDemoClick}
               className="bg-primary hover:bg-primary/90 text-primary-foreground font-medium"
             >
               Book A Demo
-            </Button> */}
+            </Button> </a>
           </div>
         </div>
       </div>
